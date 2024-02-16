bb1f4afed281356af0f4c752430f862f95cab989
Бабко Евгений <doxis32123@gmail.com>
2024-02-16 20:37:31 +0200

    initial

---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+const MainPage = () => {
+    return (
+        <Layout nickname="dupa" id={1}>
+            <div>MAIN</div>
+        </Layout>
+    );
+};
+
+export default MainPage;
