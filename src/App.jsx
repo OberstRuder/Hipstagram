41f04ff6aa90a9124d231feeb2b865f15848caf8
Бабко Евгений <doxis32123@gmail.com>
2024-03-02 23:14:02 +0200

    pg reload fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { CRouting } from './components/routing';
 import { CHeader } from './components/Header';
+import { actionAboutMe } from './redux/actions/actionsMe';
+import { actionAuthLogin } from './redux/actions/actionsAuth';
+import { actionFullGetAllPosts } from './redux/actions/actionsPost';
+import { store } from './redux/store';
 
 function App() {
+
+  if(localStorage.authToken) {
+    store.dispatch(actionAboutMe())
+    store.dispatch(actionAuthLogin(localStorage.authToken))
+    store.dispatch(actionFullGetAllPosts())
+  }
   return (
     <BrowserRouter>
       <CHeader />
